95c137f3cfc1d08641f70a0aa53be50f5353939c
Ilya Medvedev <imedvedev@readymag.com>
2021-11-25 12:21:46 +0300

    Subject encoding

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,11 @@
   windows_subsystem = "windows"
 )]
 
+use chrono::*;
+use rfc2047_decoder;
 use std::fs;
 use std::str;
+use utime::*;
 
 fn main() {
   tauri::Builder::default()
@@ -36,6 +39,10 @@ fn run_email_export(host: &str, port: u16, login: &str, password: &str, is_secur
     let mailbox_name = mailbox.name();
     imap_session.select(mailbox_name).unwrap();
 
+    let decoded_mailbox_name = mailbox_name.as_bytes();
+
+    println!("{:?} {:?}", mailbox, decoded_mailbox_name);
+
     // Create mailbox directory
     let mailbox_dir = format!("{}/{}", out_path, mailbox_name);
     fs::create_dir_all(mailbox_dir);
@@ -44,21 +51,34 @@ fn run_email_export(host: &str, port: u16, login: &str, password: &str, is_secur
     let messages = imap_session.fetch("1:3", "(RFC822 ENVELOPE)").unwrap();
     for message in messages.iter() {
       let envelope = message.envelope().unwrap();
-
-      let mut subject_string = String::new();
-      if let Some(subject) = envelope.subject {
-        subject_string.push_str(std::str::from_utf8(subject).unwrap())
-      } else {
-        println!("Message didn't have a subject!");
-      }
+      let date = get_date(std::str::from_utf8(envelope.date.unwrap()).unwrap());
+      let subject = rfc2047_decoder::decode(envelope.subject.unwrap()).expect("No subject");
 
       if let Some(body) = message.body() {
-        let filepath = format!("{}/{}/{}.eml", out_path, mailbox_name, subject_string);
-        println!("{}", filepath);
-        fs::write(filepath, body).expect("Unable to write file");
+        // Saving email as /output_path/mailbox/subject.eml
+        let filepath = format!("{}/{}/{}.eml", out_path, mailbox_name, subject);
+        fs::write(filepath.clone(), body).expect("Unable to write file");
+        set_file_times(filepath, date.timestamp(), date.timestamp()).unwrap();
       } else {
         println!("Message didn't have a body!");
       }
     }
   }
+}
+
+/** Parse date from RFC822 or RFC822 + (Zone) */
+fn get_date(s: &str) -> DateTime<FixedOffset> {
+  return match DateTime::parse_from_rfc2822(s) {
+    Ok(res) => res,
+    Err(_err) => {
+      return match DateTime::parse_from_str(s, "%a, %e %b %Y %T %z (%Z)") {
+        Ok(res) => res,
+        Err(_err) => {
+          let ndt = Local::now();
+          let dt: DateTime<FixedOffset> = ndt.to_string().parse().unwrap();
+          return dt;
+        }
+      }
+    }
+  };
 }
