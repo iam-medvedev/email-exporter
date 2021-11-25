eecc9d0756347f6bcf1829a35f36e3dcd0285a2d
Ilya Medvedev <imedvedev@readymag.com>
2021-11-25 22:36:54 +0300

    UTF-7 RFC 3501 Encoding

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,10 @@
   windows_subsystem = "windows"
 )]
 
+use base64;
 use chrono::*;
+use encoding_rs::*;
+use regex::Regex;
 use rfc2047_decoder;
 use std::fs;
 use std::str;
@@ -17,17 +20,18 @@ fn main() {
 }
 
 #[tauri::command]
-fn run_email_export(host: &str, port: u16, login: &str, password: &str, is_secured: bool) {
+/**
+ * Export emails
+ */
+fn run_email_export(host: &str, port: u16, login: &str, password: &str, out_path: &str) {
   println!(
-    "Debug: {host} {port} {login} {password} {is_secured}",
+    "Debug: {host} {port} {login} {password} {out_path}",
     host = host,
     port = port,
     login = login,
     password = password,
-    is_secured = is_secured
+    out_path = out_path
   );
-
-  let out_path = "/Users/imedvedev/Desktop/mail-export";
 
   let tls = native_tls::TlsConnector::builder().build().unwrap();
   let client = imap::connect((host, port), host, &tls).unwrap();
@@ -39,12 +43,10 @@ fn run_email_export(host: &str, port: u16, login: &str, password: &str, is_secur
     let mailbox_name = mailbox.name();
     imap_session.select(mailbox_name).unwrap();
 
-    let decoded_mailbox_name = mailbox_name.as_bytes();
-
-    println!("{:?} {:?}", mailbox, decoded_mailbox_name);
+    let decoded_mailbox_name = decode_utf7_imap(String::from(mailbox_name));
 
     // Create mailbox directory
-    let mailbox_dir = format!("{}/{}", out_path, mailbox_name);
+    let mailbox_dir = format!("{}/{}/", out_path, decoded_mailbox_name);
     fs::create_dir_all(mailbox_dir);
 
     // Getting all messages from current mailbox
@@ -56,7 +58,9 @@ fn run_email_export(host: &str, port: u16, login: &str, password: &str, is_secur
 
       if let Some(body) = message.body() {
         // Saving email as /output_path/mailbox/subject.eml
-        let filepath = format!("{}/{}/{}.eml", out_path, mailbox_name, subject);
+        let filepath = format!("{}/{}/{}.eml", out_path, decoded_mailbox_name, subject);
+
+        println!("{}", filepath);
         fs::write(filepath.clone(), body).expect("Unable to write file");
         set_file_times(filepath, date.timestamp(), date.timestamp()).unwrap();
       } else {
@@ -66,7 +70,50 @@ fn run_email_export(host: &str, port: u16, login: &str, password: &str, is_secur
   }
 }
 
-/** Parse date from RFC822 or RFC822 + (Zone) */
+/**
+ * Decode UTF-7 IMAP mailbox name
+ *
+ * https://datatracker.ietf.org/doc/html/rfc3501#section-5.1.3
+ */
+fn decode_utf7_imap(text: String) -> String {
+  let re = Regex::new(r"&[^&-]*-").unwrap();
+  let mut result = text.clone();
+
+  for cap in re.captures_iter(&text) {
+    let encoded_text = cap.get(0).map_or("", |m| m.as_str());
+    let decoded_text = decode_utf7_part(String::from(encoded_text));
+
+    result = text.replace(&encoded_text, &decoded_text);
+  }
+
+  return result;
+}
+
+/**
+ * Decode UTF-7 IMAP part of string
+ */
+fn decode_utf7_part(text: String) -> String {
+  if text == "&-" {
+    return String::from("&");
+  }
+
+  let text_mb64 = &text[1..text.len() - 1];
+  let mut text_b64 = text_mb64.replace(",", "/");
+
+  while (text_b64.len() % 4) != 0 {
+    text_b64 += "=";
+  }
+
+  let text_u16 = base64::decode(text_b64).unwrap();
+  let (cow, _encoding_used, _had_errors) = UTF_16BE.decode(&text_u16);
+  let result = cow.as_ref();
+
+  return String::from(result);
+}
+
+/**
+ * Parse date from RFC822 or RFC822 + (Zone)
+ */
 fn get_date(s: &str) -> DateTime<FixedOffset> {
   return match DateTime::parse_from_rfc2822(s) {
     Ok(res) => res,
