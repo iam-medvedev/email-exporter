1ddfafc12a5df00af2ae6d06654e57540efac4cc
Ilya Medvedev <imedvedev@readymag.com>
2021-11-26 20:31:30 +0300

    Icons

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,9 +4,17 @@
 )]
 
 mod exporter;
+use tauri::{Builder, Menu, MenuItem};
 
 fn main() {
-  tauri::Builder::default()
+  let menu = Menu::new()
+    .add_native_item(MenuItem::Quit)
+    .add_native_item(MenuItem::Copy)
+    .add_native_item(MenuItem::Cut)
+    .add_native_item(MenuItem::Paste);
+
+  Builder::default()
+    .menu(menu)
     .invoke_handler(tauri::generate_handler![exporter::run_email_export])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
